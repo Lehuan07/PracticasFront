ab8b1d5de34d78bf836ead99ec9e321c3828cfb6
Nahuel C. <nahuelncejas@gmail.com>
2025-06-28 11:04:13 -0300

    Primera parte del ejercicio calcular edad

---
+++ JS/7. Proyecto 7 - Calcular edad/assets/js/main.js
@@ -0,0 +1,12 @@
+
+function calcularEdad(){
+    let fechaValida = false;
+    let anioNacimiento;
+
+    while(!fechaValida){
+        let anioIngresado = prompt("Ingrese su fecha de nacimiento");
+        let anioEnNumeros = parseInt(anioIngresado);
+    }
+}
+
+calcularEdad();
