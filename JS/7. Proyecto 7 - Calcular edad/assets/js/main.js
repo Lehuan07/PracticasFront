c950dab2476a493ba02f6455046fa0d1b2f6604e
Nahuel C. <nahuelncejas@gmail.com>
2025-06-30 09:13:14 -0300

    Funcion + while
    Bueno, al original le agregue una funcion y un while para ir practicando

--- JS/7. Proyecto 7 - Calcular edad/assets/js/main.js
+++ JS/7. Proyecto 7 - Calcular edad/assets/js/main.js
@@ -6,7 +6,25 @@ function calcularEdad(){
     while(!fechaValida){
         let anioIngresado = prompt("Ingrese su fecha de nacimiento");
         let anioEnNumeros = parseInt(anioIngresado);
+        let anioActual = new Date().getFullYear();
+
+        if(isNaN(anioEnNumeros)){
+            alert("Ingrese un numero valido");
+        }
+        else if(anioEnNumeros <= 1919 ){
+            alert("Usted es demasiado viejo para vivir, llamen a la parca")
+        }
+        else if(anioEnNumeros > anioActual){
+            alert("Usted nacio en el futuro o que paso?")
+        }
+        else{
+            let calcular = anioActual - anioEnNumeros;
+            return calcular;
+            fechaValida = true;
+        }
     }
 }
 
-calcularEdad();
+let edadUsuario = calcularEdad();
+
+alert("La edad ingresada es: " + edadUsuario);
