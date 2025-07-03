1d17d2eb5dde5e1e4dc2fbbc0c00b181ecfbe098
Nahuel C. <nahuelncejas@gmail.com>
2025-07-03 10:55:37 -0300

    Agregar btn dinamicamente
    El btn fue de eliminar, ademas le di funcionalidades a cada boton

---
+++ JS/15. Proyecto 15 - Agregar btn a tarjeta con JS/assets/js/main.js
@@ -0,0 +1,25 @@
+
+
+let tarjetas = document.querySelectorAll(".layout__card");
+
+tarjetas.forEach(tarjeta =>{
+    let btnEliminar = document.createElement("button");
+    btnEliminar.textContent ="Eliminar Tarjeta";
+    btnEliminar.classList.add("card__btn-delate")
+
+    tarjeta.appendChild(btnEliminar);
+
+    btnEliminar.addEventListener("click", () =>{
+        tarjeta.remove();
+    });
+
+});
+
+let cabiarEstilos = document.querySelectorAll(".card__btn-change");
+
+cabiarEstilos.forEach(cambiar =>{
+
+    cambiar.addEventListener("click", () =>{
+        cambiar.parentElement.parentElement.classList.toggle("carta--oscura");
+    });
+});
