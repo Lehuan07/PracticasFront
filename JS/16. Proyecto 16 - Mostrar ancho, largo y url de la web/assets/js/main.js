de5966302dc99ae847d7194642b03ffffab2e580
Nahuel C. <nahuelncejas@gmail.com>
2025-07-04 09:45:54 -0300

    Como saber el ancho, alto, abrir paginas en blank con JS

---
+++ JS/16. Proyecto 16 - Mostrar ancho, largo y url de la web/assets/js/main.js
@@ -0,0 +1,13 @@
+
+
+let impresion = document.getElementById("info");
+let ancho = window.innerWidth;
+let alto = window.innerHeight;
+let url = window.location.href;
+
+window.open("https://google.com", "_blank");
+
+impresion.innerHTML = `
+El ancho de la pantalla es de ${ancho}px, <br>
+El alto es de ${alto}px, <br> mientras que usted
+esta en la pagina ${url}`
