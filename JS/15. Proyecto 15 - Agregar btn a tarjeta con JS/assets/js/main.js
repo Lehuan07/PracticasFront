715cda8f62f3d10693e557627b281d7238651f51
Nahuel C. <nahuelncejas@gmail.com>
2025-07-04 10:44:06 -0300

    Agregue lo del ejercicio 17

--- JS/15. Proyecto 15 - Agregar btn a tarjeta con JS/assets/js/main.js
+++ JS/15. Proyecto 15 - Agregar btn a tarjeta con JS/assets/js/main.js
@@ -1,8 +1,9 @@
 
 
-let tarjetas = document.querySelectorAll(".layout__card");
+let tarjetas = document.querySelectorAll(".botones");
 
 tarjetas.forEach(tarjeta =>{
+    
     let btnEliminar = document.createElement("button");
     btnEliminar.textContent ="Eliminar Tarjeta";
     btnEliminar.classList.add("card__btn-delate")
@@ -10,16 +11,30 @@ tarjetas.forEach(tarjeta =>{
     tarjeta.appendChild(btnEliminar);
 
     btnEliminar.addEventListener("click", () =>{
-        tarjeta.remove();
+        const layout = btnEliminar.closest(".layout__card");
+        let confirmacion = confirm("Realmente quieres eliminar?");
+        if(!confirmacion){
+
+        }
+        else{
+
+            layout.remove();
+        }
     });
 
 });
 
-let cabiarEstilos = document.querySelectorAll(".card__btn-change");
+let cambiarEstilos = document.querySelectorAll(".card__btn-change");
 
-cabiarEstilos.forEach(cambiar =>{
+cambiarEstilos.forEach(boton => {
+    boton.addEventListener("click", () => {
+        const tarjeta = boton.closest(".layout__card");
+        const header = tarjeta.querySelector(".header");
 
-    cambiar.addEventListener("click", () =>{
-        cambiar.parentElement.classList.toggle("carta--oscura");
+        // Cambiar clase general
+        tarjeta.classList.toggle("cambiar--estilos");
+
+        // Cambiar color de fondo del header
+        header.style.backgroundColor = "#1B62CA";
     });
 });
