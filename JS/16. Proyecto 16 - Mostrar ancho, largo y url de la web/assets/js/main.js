268d3cb1b1b46e7846ab79a29b81694aee6f5f68
Nahuel C. <nahuelncejas@gmail.com>
2025-07-04 10:44:19 -0300

    Le agregue un H1 para practicar

--- JS/16. Proyecto 16 - Mostrar ancho, largo y url de la web/assets/js/main.js
+++ JS/16. Proyecto 16 - Mostrar ancho, largo y url de la web/assets/js/main.js
@@ -7,7 +7,12 @@ let url = window.location.href;
 
 window.open("https://google.com", "_blank");
 
+const titulo = "Esta son las medidas de su pantalla y el URL";
+
+
+
 impresion.innerHTML = `
+<h1>${titulo}</h1><br>
 El ancho de la pantalla es de ${ancho}px, <br>
 El alto es de ${alto}px, <br> mientras que usted
 esta en la pagina ${url}`
