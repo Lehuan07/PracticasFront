a7c387c91b199cf2ea3a994a976a10439189c5fc
Nahuel C. <nahuelncejas@gmail.com>
2025-07-31 10:49:23 -0300

    Ejercicio 18 resuelto

--- JS/18 - Proyecto 18 - Creacion de un creonometro/assets/js/main.js
+++ JS/18 - Proyecto 18 - Creacion de un creonometro/assets/js/main.js
@@ -1,7 +1,53 @@
 
 
-let iniciar = document.querySelector("#iniciar");
+let segundos = 0;
+let intervalo = null;
+
+const iniciar = document.querySelector("#iniciar");
+const tiempo = document.querySelector("#tiempo");
+const parar = document.querySelector("#parar")
+
+
+
+function contadorTiempo(){
+    //*Calculamos minutos y segundos
+    const minutos = Math.floor(segundos/60);
+    const secs = segundos % 60;
+    //*Formateo los ceros y lo metemos en el tiempo
+    tiempo.textContent = `${String(minutos).padStart(2, '0')}:${String(secs).padStart(2, '0')}`
+}
+
 
 iniciar.addEventListener("click", ()=>{
-    alert("BTN INICIADO")
+    //*Evitamos multiples intervalos
+    if(intervalo) return;
+
+    //*Ponemos en funcionamiento el cronometro
+    intervalo = setInterval(() =>{
+        //*Sumamos los segundos
+        segundos++;
+        //*Si llega a 60minutos se reinicia, como lo pide el ejercicio
+    if (segundos >= 3600) {
+        //*Reiniciamos todo y largamos un alerta
+        clearInterval(intervalo);
+        intervalo = null;
+        alert("¡Llegaste al máximo de 60 minutos!");
+        return;
+    }else{
+        //*Si no llega a 60min llamamos a contadorTiempo
+        contadorTiempo();
+        }
+    }, 1000);
+})
+
+
+//*Paramos el tiempo (ENA!)
+parar.addEventListener("click", ()=>{
+    //*Limpiamos el intervalo. 
+    clearInterval(intervalo)
+    intervalo = null;
+    //* Reinicimos los segundos a cero y volvemo 
+    segundos = 0;
+    //* a mostrar el tiempo en 00:00 con contadorTiempo().
+    contadorTiempo();
 })
