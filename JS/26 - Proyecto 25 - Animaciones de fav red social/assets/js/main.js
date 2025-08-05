04a43f5c2f7148531860bb688a796532fb5d697e
Nahuel C. <nahuelncejas@gmail.com>
2025-08-05 11:00:16 -0300

    Efecto agregar Fav

---
+++ JS/26 - Proyecto 25 - Animaciones de fav red social/assets/js/main.js
@@ -0,0 +1,14 @@
+
+
+let emoji = document.querySelector(".emojis__emoji")
+
+
+emoji.addEventListener("click", () => {
+  if (emoji.classList.contains("emojis__emoji")) {
+    emoji.classList.remove("emojis__emoji");
+    emoji.classList.add("fav");
+  } else {
+    emoji.classList.remove("fav");
+    emoji.classList.add("emojis__emoji");
+  }
+});
