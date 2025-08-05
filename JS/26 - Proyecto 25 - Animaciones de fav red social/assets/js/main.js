ed5d5602996918325d29b12ae7de01b219decf4c
Nahuel C. <nahuelncejas@gmail.com>
2025-08-05 11:01:53 -0300

    Pequeño arreglo

--- JS/26 - Proyecto 25 - Animaciones de fav red social/assets/js/main.js
+++ JS/26 - Proyecto 25 - Animaciones de fav red social/assets/js/main.js
@@ -4,11 +4,12 @@ let emoji = document.querySelector(".emojis__emoji")
 
 
 emoji.addEventListener("click", () => {
-  if (emoji.classList.contains("emojis__emoji")) {
+    
+    if (emoji.classList.contains("emojis__emoji")) {
     emoji.classList.remove("emojis__emoji");
     emoji.classList.add("fav");
-  } else {
+    } else {
     emoji.classList.remove("fav");
     emoji.classList.add("emojis__emoji");
-  }
+    }
 });
