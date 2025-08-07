185acbdac9379afba761125cd99d5c4fcd574822
Nahuel C. <nahuelncejas@gmail.com>
2025-08-07 09:53:11 -0300

    Loader con JS con efecto visual

--- JS/28 - Proyecto 28 - Transicion de 0 a 100 carga/assets/js/main.js
+++ JS/28 - Proyecto 28 - Transicion de 0 a 100 carga/assets/js/main.js
@@ -1,13 +1,38 @@
 
 
 let boton = document.querySelector(".container__btn")
-
+let texto = document.querySelector(".disolver")
+let porcentaje = document.querySelector(".container__load")
 const blurred  = document.querySelector(".background--blurred")
 const sharp = document.querySelector('.background--sharp');
 
 
 
+
 boton.addEventListener("click", ()=>{
-    blurred.style.opacity = '0';
-    sharp.classList.remove('oculto');
+    
+    let i = 0;
+    let blurLevel = 5;
+    // Carga progresiva
+    const carga = setInterval(() => {
+
+        if (blurLevel <= 0) {
+                blurred.style.display = "none"; 
+            } else {
+                blurLevel -= 0.05;
+                blurred.style.filter = `blur(${blurLevel.toFixed(2)}px)`;
+            }
+        if (i <= 100) {
+        porcentaje.innerHTML = `${i}%`;
+        i++;
+        } else {
+            clearInterval(carga);
+            
+            boton.classList.add("disolver");
+            texto.classList.remove("disolver");
+            texto.classList.add("container__text");
+            sharp.classList.remove("oculto");
+        }
+}, 50);
+
 })
