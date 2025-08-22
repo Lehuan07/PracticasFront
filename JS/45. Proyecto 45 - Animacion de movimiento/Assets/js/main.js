3c40022b2e78e3acc08bede02350902834593249
Lehuan07 <nahuelncejas@gmail.com>
2025-08-21 21:54:33 -0300

    Logrado

--- JS/45. Proyecto 45 - Animacion de movimiento/Assets/js/main.js
+++ JS/45. Proyecto 45 - Animacion de movimiento/Assets/js/main.js
@@ -2,10 +2,12 @@ let layout = document.querySelector(".layout");
 let upbtn = document.querySelector(".btn__up");
 let downbtn = document.querySelector(".btn__down");
 let left = document.querySelector(".layout__left");
-let right = document.querySelector(".layout__right");
+let right = document.querySelector(".layout__rigth");
 
 let sliderLength = document.querySelectorAll(".left__bg").length;
 let sliderIndex = 0;
+
+right.style.top = `-${(sliderLength - 1) * 100}vh`;
 
 let actions = (action) => {
     if(action === "up"){
@@ -21,8 +23,8 @@ let actions = (action) => {
     }
 
     let windowHeight = layout.clientHeight;
-    left.style.transform = `translateY(-${sliderIndex * windowHeight}px)`;
-    right.style.transform = `translateY(-${sliderIndex * windowHeight}px)`;
+    left.style.transform = `translateY(-${(sliderIndex * windowHeight)}px)`;
+    right.style.transform = `translateY(${(sliderIndex * windowHeight)}px)`;
 }
 
 upbtn.addEventListener("click", () => {
